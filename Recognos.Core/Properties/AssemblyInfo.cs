b88410682d22231eac3fb78ad51e1367d3d190b6
Iulian Margarintescu <iulian@recognos.com>
2014-01-17 17:15:32 +0200

    bump version

--- Recognos.Core/Properties/AssemblyInfo.cs
+++ Recognos.Core/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
